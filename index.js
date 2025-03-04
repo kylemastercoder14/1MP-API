f5ed2e8fc34889b2bc4d0bda753b24f240dfbd10
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-03-04 21:47:45 +0800

    vercel

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.use(express.json());
 
 app.use("/api/v1/categories", require("./routes/categories-route"));
 app.use("/api/v1/sub-categories", require("./routes/subcategories-route"));
+app.get("/", (req, res) => {
+  res.send("Developed by: ~kylemastercoder~");
+});
 
 app.listen(6969, () => {
   console.log("Server is running on port 6969");

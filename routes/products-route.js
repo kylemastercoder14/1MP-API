39352e294b50bb19c2798f23c0346f5ce993f010
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-03-14 06:48:03 +0800

    fixed issue

--- routes/products-route.js
+++ routes/products-route.js
@@ -3,12 +3,15 @@ const productController = require("../controllers/products-controller");
 
 router.post("/", productController.createProduct);
 router.get("/", productController.getAllProducts);
-router.get("/:id", productController.getProductById);
-router.get("/slug/:slug", productController.getProductBySlug);
+
+// Place these BEFORE the dynamic `/:id` route
 router.get("/categories/:slug", productController.getProductByCategorySlug);
 router.get(
   "/sub-categories/:slug",
   productController.getProductBySubCategorySlug
 );
+
+router.get("/slug/:slug", productController.getProductBySlug);
+router.get("/:id", productController.getProductById);
 
 module.exports = router;
